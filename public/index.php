d73163c8ab5185087e47b0a6ba9dde8d70e5d2ed
Jakub Orawczyk <jakuboraw@gmail.com>
2025-01-04 17:46:30 +0100

    added script into script.js instead

--- public/index.php
+++ public/index.php
@@ -33,34 +33,6 @@
 
     <div id="response"></div>
 
-    <script>
-        // Handle form submission via AJAX to avoid page reload
-        document.getElementById('bookingForm').addEventListener('submit', function(event) {
-            event.preventDefault();
-
-            const formData = new FormData(this);
-            fetch('booking.php', {
-                method: 'POST',
-                body: formData
-            })
-            .then(response => response.json())
-            .then(data => {
-                const responseDiv = document.getElementById('response');
-                if (data.status === 'success') {
-                    responseDiv.innerHTML = `<p>Booking Success! Total Cost: ${data.booking_details.total_cost}</p>`;
-                    responseDiv.innerHTML += `<p>Features:</p><ul>`;
-                    data.booking_details.features.forEach(feature => {
-                        responseDiv.innerHTML += `<li>${feature.name} - Cost: ${feature.cost}</li>`;
-                    });
-                    responseDiv.innerHTML += `</ul>`;
-                } else {
-                    responseDiv.innerHTML = `<p>Error: ${data.message}</p>`;
-                }
-            })
-            .catch(error => {
-                document.getElementById('response').innerHTML = `<p>Error: Unable to process the booking</p>`;
-            });
-        });
-    </script>
+    <script src="../src/script.js"></script>
 </body>
 </html>
