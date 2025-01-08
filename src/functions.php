5edcf76bc8ebf1a2e7c2abf29b0a4bd4a9e3aa5c
Jakub Orawczyk <jakuboraw@gmail.com>
2025-01-08 16:00:43 +0100

    added better comments to function

--- src/functions.php
+++ src/functions.php
@@ -3,8 +3,8 @@ function getFeatureCost($featureName) {
     $featureCosts = [
         "Pool Access" => 3,
         "Breakfast" => 5,
-        "Gym Access" => 3,
-        // Add more features and their costs here
+        "Gym Access" => 3, 
+        // Add more features and their costs underneath
     ];
 
     return $featureCosts[$featureName] ?? 0; // Return 0 if feature not found
